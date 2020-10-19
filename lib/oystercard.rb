1945b38df070a59b8752f4520ac239737fcecf28
Melissa <mkneale1@hotmail.com>
2020-10-19 18:11:46 +0100

    Day 1 Commit Complete

---
+++ lib/oystercard.rb
@@ -0,0 +1,16 @@
+class Oystercard
+
+  MAXIMUM_BALANCE = 90
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    fail ("Maximum balance of 90 exceeded") if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,21 @@
+require 'oystercard'
+
 RSpec.describe Oystercard do
-  
+
+  it { is_expected.to respond_to('balance') }
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'raises an error if limit is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up maximum_balance
+      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+
+  end
 end
