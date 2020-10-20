663b8b832ca8e6f76290e0f0e61ce365b42a6efa
Melissa <mkneale1@hotmail.com>
2020-10-20 18:22:02 +0100

    End of Tuesday Commit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,7 +2,7 @@ class Oystercard
 
   MAXIMUM_BALANCE = 90
 
-  attr_reader :balance
+  attr_accessor :balance, :journey
 
   def initialize
     @balance = 0
@@ -11,6 +11,23 @@ class Oystercard
   def top_up(amount)
     fail ("Maximum balance of 90 exceeded") if amount + balance > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def deduct_money(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    false
+    @journey
+  end
+
+  def touch_in
+    @journey = true if @balance > 0
+  end
+
+  def touch_out
+    @journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,10 +12,35 @@ RSpec.describe Oystercard do
     end
 
     it 'raises an error if limit is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      maximum_balance = Oystercard::MAXIMUM_BALANCE #gets variable from class
       subject.top_up maximum_balance
       expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
-
   end
+
+    describe '#deduct_money' do
+      it 'deducts fare from balance' do
+        expect{ subject.deduct_money 1 }.to change{ subject.balance }.by -1
+      end
+    end
+
+    it 'is initially not in journey' do # in journey is set to false as standard
+      expect(subject).not_to be_in_journey
+    end
+
+    # touch in method should cause @journey to be set to true
+
+    # need tests to require balance to be topped up
+
+    it 'can touch in' do
+      subject.top_up(50) # putting balance on oystercard in order to be able to tap in
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'can touch out' do
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
 end
